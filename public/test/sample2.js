84e517c523c0527954c2165bc17460dcea1dc771
rock's ezweb <alikerock@gmail.com>
2023-10-16 10:22:48 +0900

    module export import

---
+++ public/test/sample2.js
@@ -0,0 +1,4 @@
+import {langs, hello1, hello2, hello3} from "sample1s";
+import lang from 'langs';
+
+console.log(langs,hello1, hello2, hello3, lang);
--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 function App() {  
   return (
     <div className="App">
-   
-    </div>
-  );
+      <h1>Welcome to Hangman!</h1>
+      <p>Do you want to play game?</p>
+    </div> 
+  );  
 }
 
 export default App;
