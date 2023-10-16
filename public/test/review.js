1682379d3fb0803e71368f280e40ee58c46a5e78
rock's ezweb <alikerock@gmail.com>
2023-10-16 12:46:00 +0900

    setguessedletters

--- public/test/review.js
+++ public/test/review.js
@@ -17,3 +17,19 @@ let list4 = arr.map(item=> `<li>${item}</li>`);
 
 
 console.log(list);
+
+
+let arr1 = [1,2];
+arr1.push(3);
+
+let arr1es6 = [...arr1, 3];
+
+let arr2 = [4,5];
+
+let arrnew = [arr1[0],arr1[1],arr2[0],arr2[1]];
+
+let arrnewEs6 = [...arr1, ...arr2];
+
+let arrnew1 = arr1.concat(arr2);
+
+let arrnew2 = [].concat(arr1,arr2);
--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,12 +1,20 @@
 import LetterGrid from "./LetterGrid";
 import ButtonGrid from "./ButtonGrid";
+import { useState } from "react";
 
 const GameBoard = ({secretWord})=>{
+  const [guessedLetters, setGuessedLetters] = useState([]);
 
-  
+  const letterGuessedHandler = (letter)=>{
+    let val = letter.toLowerCase();
+
+    setGuessedLetters(prev=> [...prev,val]);
+
+  }
+
   return(
     <div>
-      <LetterGrid secretWord={secretWord} guessedLetters={['r','e']}/>
+      <LetterGrid secretWord={secretWord} guessedLetters={guessedLetters}/>
       <ButtonGrid letterGuessed={letterGuessedHandler}/>
     </div>
   )
